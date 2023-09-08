4be149c2e61e9505a72f12ed8f4a2df424298e2f
rauls <raulsangut99@gmail.com>
2023-09-08 18:55:30 +0200

    Cluster

--- src/main/scala/raulsantos/funciones.scala
+++ src/main/scala/raulsantos/funciones.scala
@@ -44,6 +44,22 @@ object funciones{
                 sales
         }
 
+        def dfSalesExtended(spark: SparkSession): DataFrame = {
+
+                val sales = spark.read
+                  .option("header", "false")
+                  .option("delimiter", "\u0001")
+                  .csv("hdfs://nameservice1/user/hive/warehouse/TFM_raulsantos.db/sales/sales_extended_2GB.csv")
+                  .selectExpr("CAST(_c0 AS INT) as order_id",
+                          "CAST(_c1 AS INT) as product_id",
+                          "CAST(_c2 AS INT) as seller_id",
+                          "CAST(_c3 AS DATE) as date",
+                          "CAST(_c4 AS INT) as num_pieces_sold",
+                          "_c5 AS bill_raw_text",
+                          "CAST(_c6 AS INT) as product_id_num")
+                sales
+        }
+
         def dfSellers(spark:SparkSession):DataFrame={
 
                 val sellers=spark.read
